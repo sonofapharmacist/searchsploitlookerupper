cefe96d653c016b45dc894870183939ebc2df474
GP <gpagel@wipfli.com>
2018-07-10 11:06:33 -0500

    removed errors because i failed to close an if

--- searchsploit.sh
+++ searchsploit.sh
@@ -6,28 +6,10 @@ echo
 echo -n "Enter the location of your cve file: "
 read -e location
 
-# Check for no answer
-if [[ -z $location ]]; then
-     echo "you messed up"
-
-# Check for wrong answer
-if [ ! -f $location ]; then
-     echo "you messed up"
-fi
 
 echo
 echo -n "Enter the location where you want the results: "
 read -e results
-
-# Check for no answer
-if [[ -z $results ]]; then
-     echo "you messed up"
-fi
-
-# Check for wrong answer
-if [ ! -f $results ]; then
-     echo "you messed up"
-fi
 
 
 touch $results
